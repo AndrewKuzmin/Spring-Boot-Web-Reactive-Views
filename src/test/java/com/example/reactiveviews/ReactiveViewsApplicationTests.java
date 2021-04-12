d8d32cdb1f013493ce8058788b75c8f7402eb4cb
Andrew Kuzmin <andrew.kuzmin@gmail.com>
2021-04-12 15:08:53 +0300

    Upgraded to Gradle 7.0 and Spring Boot 2.4.4.

---
+++ src/test/java/com/example/reactiveviews/ReactiveViewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactiveviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ReactiveViewsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
